e3bae1e871deec8a07fe51629b0762628fd88f1b
Vinitha28-vini <vinithads2002@gmail.com>
2025-07-12 20:49:23 +0530

    first commit

---
+++ src/main/java/com/example/AwsExample/Controller/Pathcontroller.java
@@ -0,0 +1,15 @@
+package com.example.AwsExample.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Pathcontroller {
+
+	@GetMapping("/")
+		public String home() {
+			return "index";
+		}
+	
+
+}
---
+++ src/test/java/com/example/AwsExample/AwsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AwsExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
